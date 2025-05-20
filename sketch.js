00e0a171bd6c888eaef6008399968acc32b45f1a
gioiasophie <gioia.deselaers@fh-potsdam.de>
2025-05-20 14:03:01 +0200

    wieso funktionieren diese wellen einfach nicht??

--- sketch.js
+++ sketch.js
@@ -7,10 +7,6 @@ let p5bezier;
 const sketch_height = 400;
 const sketch_width = 400;
 
-let waveHeight = 100;
-let waveLength = 100;
-let segments = width / waveLength;
-
 function preload(){
   // preload assets
 }
@@ -39,31 +35,18 @@ function setup() {
 function draw() {
   background("lightblue");
 
-  for (let i = 0; i < segments; i++) {
-    let x1 = i * waveLength;
-    let x2 = x1 + waveLength;
-    let cp1 = createVector(x1 + waveLength / 3, -waveHeight);
-    let cp2 = createVector(x1 + 2 * waveLength / 3, waveHeight);
-    bezierVertex(cp1.x, cp1.y, cp2.x, cp2.y, x2, 0);
-  }
-
+  let waveHeight = sketch_height/2;
+  let waveLength = 200;
+  let segments = sketch_width/ waveLength;
+  
   const level = amp.getLevel() * 3000;
 
-  p5bezier.draw([
-    [0, sketch_height/2],
-    [level, level],
-    [400, sketch_height/2],
-  ])
-
-  let spectrum = fft.analyze(32);
-  for (let i = 0; i < spectrum.length; i++){
-    let x = map(i, 0, spectrum.length, 0, width);
-    let h = -height + map(spectrum[i], 0, 255, height, 0);
-   
+  for (let i = 0; i < sketch_width; i++) {
     p5bezier.draw([
-    [0, sketch_height/2],
-    [x, h],
-    [400, sketch_height/2],
-  ], 'OPEN')
+      [segments, waveHeight],
+      [segments += i, level],
+      [segments += i, level]
+      
+    ])
   }
 }
