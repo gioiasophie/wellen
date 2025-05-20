398a9d2f4719c5e1f9b950d5bea5416a0433d9d1
gioiasophie <gioia.deselaers@fh-potsdam.de>
2025-05-20 12:55:39 +0200

    das funktioniert einfach nicht

--- sketch.js
+++ sketch.js
@@ -2,10 +2,14 @@ let audio;
 let amp;
 let fft;
 
-let p5bezier
+let p5bezier;
 
 const sketch_height = 400;
 const sketch_width = 400;
+
+let waveHeight = 100;
+let waveLength = 100;
+let segments = width / waveLength;
 
 function preload(){
   // preload assets
@@ -13,17 +17,53 @@ function preload(){
 
 function setup() {
   let c = createCanvas(sketch_width, sketch_height);
-  let p5bezier = initBezier(c)
+  p5bezier = initBezier(c)
+
+  getAudioContext().suspend();
+  userStartAudio();
+
+  audio = new p5.AudioIn();
+  audio.start();
+
+  amp = new p5.Amplitude;
+  amp.setInput(audio);
+
+  fft = new p5.FFT();
+  fft.setInput(audio);
 
   noFill();
-  strokeWeight(5);
+  strokeWeight(3);
 
 }
 
 function draw() {
-  background("white");
+  background("lightblue");
 
-  t += 0.015
-  p5bezier.draw(getSinPoints(p, t), 'OPEN', 3);
-  
+  for (let i = 0; i < segments; i++) {
+    let x1 = i * waveLength;
+    let x2 = x1 + waveLength;
+    let cp1 = createVector(x1 + waveLength / 3, -waveHeight);
+    let cp2 = createVector(x1 + 2 * waveLength / 3, waveHeight);
+    bezierVertex(cp1.x, cp1.y, cp2.x, cp2.y, x2, 0);
+  }
+
+  const level = amp.getLevel() * 3000;
+
+  p5bezier.draw([
+    [0, sketch_height/2],
+    [level, level],
+    [400, sketch_height/2],
+  ])
+
+  let spectrum = fft.analyze(32);
+  for (let i = 0; i < spectrum.length; i++){
+    let x = map(i, 0, spectrum.length, 0, width);
+    let h = -height + map(spectrum[i], 0, 255, height, 0);
+   
+    p5bezier.draw([
+    [0, sketch_height/2],
+    [x, h],
+    [400, sketch_height/2],
+  ], 'OPEN')
+  }
 }
